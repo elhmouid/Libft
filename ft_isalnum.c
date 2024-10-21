c8bd0718c26383d933b43feef8d102006239f0eb
nothingisavailablehh <119079223+nothingisavailablehh@users.noreply.github.com>
2024-10-21 22:00:45 +0100

    in case wsafi

---
+++ ft_isalnum.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */ 
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-hmo <moel-hmo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 12:26:44 by moel-hmo          #+#    #+#             */
+/*   Updated: 2024/10/21 15:35:05 by moel-hmo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_isalpha(int c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+int ft_isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
+int ft_isalnum(int c)
+{
+    return (ft_isalpha(c) || ft_isdigit(c));
+}
+
+int main()
+{
+    printf("=========>%d\n", ft_isalnum('a'));
+}
